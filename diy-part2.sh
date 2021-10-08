9d6839361b21b4b1a8b64e734ff49f57b6a34e17
LadyMonster <89981411+393435992@users.noreply.github.com>
2021-10-08 11:56:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3|' target/linux/bcm53xx/image/Makefile
 
 # 更改默认主题
 rm -rf package/lean/luci-theme-argon
