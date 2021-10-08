84afe91d0aa70193c5d479dd179f5dd0db8d0dc0
LadyMonster <89981411+393435992@users.noreply.github.com>
2021-10-08 21:59:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
 
 # 更改默认主题
 rm -rf package/lean/luci-theme-argon
