49e5a423e7b9bf67ff297ff08a95c51036fa241c
LadyMonster <89981411+393435992@users.noreply.github.com>
2022-05-05 21:27:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,8 +17,6 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 sed -i "s/hostname='OpenWrt'/hostname='Phicomm-K3'/g" package/base-files/files/bin/config_generate
 
 # Change Default Theme
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # Add Screenctrl
 rm -rf package/lean/k3screenctrl
